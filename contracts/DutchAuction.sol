19ebb857cf80128e4aff7712bd72ba3357eca4d6
Zac Mitton <zacmitton22@gmail.com>
2017-09-26 17:05:00 -0400

    first draft complete - doesnt compile

--- contracts/DutchAuction.sol
+++ contracts/DutchAuction.sol
@@ -7,187 +7,104 @@ import "Tokens/AbstractToken.sol";
 contract DutchAuction {
     event BidSubmission(address indexed sender, uint256 amount);
 
-    uint constant public MAX_ETH_RAISED = 1000000 * 10**18; // 1.5M ETH (~$500M for 30%)
-    uint constant public WAITING_PERIOD = 1 days;
-
-    Token public trueToken;
+    uint constant WEI_PER_ETH = 10**18;
+    uint constant BLOCKS_PER_DAY = 5760;
+    uint multiplier;
+    uint tokensForSale;
+    Token public token;
     address public wallet;
-    // address public owner;
-    uint public ceiling;
-    uint public priceFactor;
     uint public startBlock;
-    uint public endTime;
+    uint public endBlock;
     uint public totalReceived;
-    uint public finalPrice;
+    uint public finalPriceReciprical; // atomic tokens per wei- to ensure whole numbers
     mapping (address => uint) public bids;
     Stages public stage;
 
-    enum Stages {/*AuctionDeployed,*/AuctionSetUp, AuctionStarted, AuctionEnded, TradingStarted }
+    enum Stages {AuctionDeployed, AuctionStarted, AuctionEnded}
 
-    modifier atStage(Stages _stage){ require(stage != _stage) _; }
-    modifier isOwner(){ require(msg.sender != owner); _;}
-    modifier isWallet(){ require(msg.sender != wallet) _; }
+    //z: note that this will update the stage before checking
+    modifier isAtStage(Stages _stage){ if(updateAndReturnStage() == _stage) _; }
 
-    // modifier isValidPayload(address receiver){
-    //     require( msg.data.length != 4 && msg.data.length != 36
-    //         || receiver == address(this)
-    //         || receiver == address(trueToken))
-    //     _;
-    // }
-
-    modifier timedTransitions(){ // will close auction if conditions are met
-        if (stage == Stages.AuctionStarted && calcTokenPrice() <= calcMinTokenPrice())
-            _finalizeAuction();
-        if (stage == Stages.AuctionEnded && now > endTime + WAITING_PERIOD)
-            stage = Stages.TradingStarted;
-        _;
-    }
-
-// gnosis params for reference:
-// address:      1d0DcC8d8BcaFa8e8502BEaEeF6CBD49d3AFFCDC
-// _wallet:      851b7f3ab81bd8df354f0d7640efcd7288553419
-// _ceiling:     34f086f3b33b68400000 // 250,000 ETH
-// _priceFactor: 1194
-
-    function DutchAuction(address _wallet, uint _ceiling, uint _priceFactor, address _trueToken, uint _startBlock){
-        // if (_wallet == 0 || _ceiling == 0 || _priceFactor == 0) { revert(); } // Arguments null
-        // if (_trueToken == 0){ revert(); } // Argument null
-        // if (trueToken.balanceOf(this) != MAX_TOKENS_SOLD) { revert(); } // Validate token balance
-        // z:note I dont think any of the above checks were actually needed
-        trueToken = Token(_trueToken);
-        // owner = msg.sender;
+    function DutchAuction(
+        address _wallet,
+        uint _weiFloor,
+        address _token,
+        uint _startBlock,
+        uint usdTargetAfter1Day,
+        uint usdPerEth
+    ){
         wallet = _wallet;
-        ceiling = _ceiling;
-        priceFactor = _priceFactor;
+        weiFloor = _weiFloor;
+        token = Token(_token);
+        tokensForSale = token.balanceOf(this);
         startBlock = _startBlock;
-        // stage = Stages.AuctionDeployed;
-        stage = Stages.AuctionSetUp;
+        multiplier = _usdTargetAfter1Day * WEI_PER_ETH * BLOCKS_PER_DAY / _usdPerEth;
+        stage = Stages.AuctionStarted;
     }
-
-    /// @dev Setup function sets external contracts' addresses
-    /// @param _trueToken TrueBit token address
-    // function setup(Token _trueToken) isOwner atStage(Stages.AuctionDeployed){}
-
-    /// @dev Starts auction and sets startBlock
-    // function startAuction() isWallet atStage(Stages.AuctionSetUp){
-    //     stage = Stages.AuctionStarted;
-    //     startBlock = block.number;
-    // }
-
-    /// @dev Changes auction ceiling and start price factor before auction is started
-    /// @param _ceiling Updated auction ceiling
-    /// @param _priceFactor Updated start price factor
-    // function changeSettings(uint _ceiling, uint _priceFactor)
-    //     isWallet
-    //     atStage(Stages.AuctionSetUp)
-    // {
-    //     ceiling = _ceiling;
-    //     priceFactor = _priceFactor;
-    // }
 
     /// @dev Returns correct stage, 
-    /// even if a function with timedTransitions modifier has not yet been called yet
+    /// even if a function with updateStage modifier has not been called yet
     /// @return Returns current auction stage
-    function updateStage() timedTransitions returns (Stages){ return stage; }
+    function updateAndReturnStage() returns(Stages){
+        if (stage == Stages.AuctionDeployed && block.number > startBlock){
+            stage = Stages.AuctionStarted;
+        }
+        if (stage == Stages.AuctionStarted){
+            if(totalReceived >= weiCap() || weiCap() <= weiFloor){
+                stage = Stages.AuctionEnded;
+            }
+        }
+        return stage;
+    }
 
     /// @dev Allows to send a bid to the auction
     /// @param receiver Bid will be assigned to this address if set
-    function bid(/*address receiver*/)
-        payable
-        // isValidPayload(msg.sender)
-        timedTransitions
-        atStage(Stages.AuctionStarted)
-        // returns (uint amount) //z: why return for external tx?
-    {
-        // If a bid is done on behalf of a user via ShapeShift, the receiver address is set
-        // if (receiver == 0){ receiver = msg.sender; }
-        uint amount = _maxWeiValue();
+    function bid() payable isAtStage(Stages.AuctionStarted){
+        // returned (uint amount) //z: why return for external tx?
+        uint maxWei = weiCap() - totalReceived;
 
-        // Prevent that more than 90% of tokens are sold. Only relevant if cap not reached
-        // deal with above comment. not sure what our analog is^^
-        if (msg.value >= amount) {
-            if(msg.value > amount){ msg.sender.transfer(msg.value - amount); }
-            _bid(amount);
-            _finalizeAuction();
-        } else{
-            _bid(amount);
+        if (msg.value >= maxWei) {
+            if(msg.value > maxWei)
+                msg.sender.transfer(msg.value - maxWei);
+            _bid(maxWei);
+            stage = Stages.AuctionEnded;
+        } else{// (msg.value < maxWei)
+            _bid(msg.value);
         }
-        // When maxWei is equal to the big amount 
-        // the auction is ended and _finalizeAuction is triggered
-        BidSubmission(msg.sender, amount);
     }
+
+    /// @dev Claims tokens for bidder after auction
+    /// @param receiver Tokens will be assigned to this address if set
+    function claimTokens() isAtStage(Stages.AuctionEnded){
+        // finalPrice = totalReceived / tokensForSale;
+        uint tokenCount = bids[msg.sender] * totalReceived / tokensForSale;
+        bids[msg.sender] = 0;
+        token.transfer(msg.sender, tokenCount);
+    }
+
+//read only
+    function weiCap() constant returns(uint){
+        return MULTIPLIER / (block.number - startBlock);
+    }
+    // sanity check, remove after testing and create a js helper
+    function usdCap() constant returns(uint){
+        return weiCap() * 300 / WEI_PER_ETH; // 300ish
+    }
+
+//private
     function _bid(uint amount) private{
         // Forward funding to ether wallet
         wallet.transfer(amount);
         bids[msg.sender] += amount;
         totalReceived += amount;
-    }
-
-    function _maxWeiValue() private returns(uint maxWei){
-        maxWei = (trueToken.balanceOf(this) / 10**18) * calcTokenPrice() - totalReceived;
-        uint maxWeiBasedOnTotalReceived = ceiling - totalReceived;
-        if (maxWeiBasedOnTotalReceived < maxWei){ maxWei = maxWeiBasedOnTotalReceived; }
-    }
-
-    /// @dev Claims tokens for bidder after auction
-    /// @param receiver Tokens will be assigned to this address if set
-    function claimTokens(/*address receiver*/)
-        // isValidPayload(receiver)
-        timedTransitions
-        atStage(Stages.TradingStarted)
-    {
-        // if (receiver == 0){ receiver = msg.sender; }
-        uint tokenCount = bids[msg.sender] * 10**18 / finalPrice;
-        bids[msg.sender] = 0;
-        trueToken.transfer(msg.sender, tokenCount);
-    }
-
-//read only
-    /// @dev Calculates stop price
-    /// @return Returns stop price
-    function calcMinTokenPrice() constant returns (uint){
-        return totalReceived * 10**18 / trueToken.balanceOf(this) + 1;
-    }
-
-    // z:why is this function here. It never gets called and is not part of the 
-    // API defined by DutchAuction abstraction
-    /// @dev Calculates current token price
-    /// @return Returns token price
-    // function calcCurrentTokenPrice() timedTransitions returns (uint) {
-    //     if (stage == Stages.AuctionEnded || stage == Stages.TradingStarted)
-    //         return finalPrice;
-    //     return calcTokenPrice();
-    // }
-
-    /// @dev Calculates token price
-    /// @return Returns token price
-
-    //z: this function should be set at deployment (all factors remain constant); edit: WHAt??? no
-    // why the plus 1? pf is set as positive int. why + 7500?
-    // prices seem to be in units of wei/GNO.
-    function calcTokenPrice() constant returns (uint){
-        return priceFactor * 10**18 / (block.number - startBlock + 7500) + 1;
-    }
-
-//private
-    function _finalizeAuction() private {
-        stage = Stages.AuctionEnded;
-        if (totalReceived == ceiling)
-            finalPrice = calcTokenPrice();
-        else
-            finalPrice = calcMinTokenPrice();
-        uint soldTokens = totalReceived * 10**18 / finalPrice;
-        // Auction contract transfers all unsold tokens to TrueBit inventory multisig
-        //z: which we dont need
-        // trueToken.transfer(wallet, MAX_TOKENS_SOLD - soldTokens);
-        //z: do we need this? do we need the distinction `TradingStarted`? thoughts: seems not
-        endTime = now;
+        BidSubmission(msg.sender, amount);
     }
 }
 
 
 //z: try to adhere to the API
-// /// @title Abstract dutch auction contract - Functions to be implemented by dutch auction contracts
+// /// @title Abstract dutch auction contract - Functions to be implemented 
+// /// by dutch auction contracts
 // contract DutchAuction {
 
 //     function bid(address receiver) payable returns (uint);
@@ -197,17 +114,89 @@ contract DutchAuction {
 //     Token public gnosisToken;
 // }
 
+    // /// @dev Calculates stop price
+    // /// @return Returns stop price
+    // function calcMinTokenPrice() constant returns (uint){
+    //     return totalReceived * 10**18 / token.balanceOf(this) + 1;
+    // }
+
+    // z:why is this function here. It never gets called and is not part of the 
+    // API defined by DutchAuction abstraction
+    /// @dev Calculates current token price
+    /// @return Returns token price
+    // function calcCurrentTokenPrice() updateStage returns (uint) {
+    //     if (stage == Stages.AuctionEnded || stage == Stages.TradingStarted)
+    //         return finalPriceReciprical;
+    //     return calcTokenPrice();
+    // }
+
+    /// @dev Calculates token price
+    /// @return Returns token price
+
+    //z: this function should be set at deployment (all factors remain constant); edit: WHAt??? no
+    // why the plus 1? pf is set as positive int. why + 7500?
+    // prices seem to be in units of wei/GNO.
+    // function calcTokenPrice() constant returns (uint){
+    //     return priceFactor * 10**18 / (block.number - startBlock + 7500) + 1;
+    // }
+
+        // if (totalReceived == ceiling)
+        // else
+            // finalPriceReciprical = calcMinTokenPrice();
+        // uint soldTokens = totalReceived * 10**18 / finalPriceReciprical;
+        // Auction contract transfers all unsold tokens to Tokens inventory multisig
+        //z: which we dont need
+        // token.transfer(wallet, MAX_TOKENS_SOLD - soldTokens);
+        //z: do we need this? do we need the distinction `TradingStarted`? thoughts: seems not
+        // endTime = now;
+
+    // function _finalizeAuction() private {
+    //     require(totalReceived != 0);
+    //     stage = Stages.AuctionEnded;
+    //     finalPriceReciprical = token.balanceOf(this) / totalReceived;
+    //     price rounded up (because finalPriceReciprical rounded down)
+    // }
+
 
 // SCHEDULE
 // Wk-> $M
 // ===========
-// 0 -> 500
-// 1 -> 250
-// 2 -> 125
-// 3 ->  62.5
-// 4 ->  31.25
-// 5 ->  15.xx
-// 6 ->   7.xx
+// Might want to start at a billion just to be safe (should start at an impossible number)
+
+// 0  -> infinite
+// 1  -> 1000
+// 2  ->  500
+// 3  ->  333
+// 4  ->  250
+// 5  ->  199
+// 6  ->  166
+// 7  ->  142
+// 8  ->  125
+// 9  ->  111
+// 10 ->  100
+// 11 ->   90
+// 12 ->   83
+// 13 ->   76
+// 14 ->   71
+// 15 ->   66
+// 16 ->   62
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 32 ->   31.25
+
+// call it 6 weeks. aim for 4. aim for a full week between 125 and 62
 
